5a4d713592d5828f14ae275f83eda3bfaa0a6d0e
Ricardo Castillo <ricardoacz20@gmail.com>
2023-11-13 10:53:44 -0500

    Simple search page to find yugioh cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'; 
+import axios from 'axios'
 
 function App() {
+  // Setting out updatables variable
+  const [name, setName] = useState("")
+  const [card, setCard] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  // Functions to update stuff
+  function handleChange (e) {
+    setName(e.target.value)
+  }
+
+  // API request
+  function handleSubmit (e) {
+    e.preventDefault()
+    setLoading(true)
+    axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${name}`)
+      .then((response) => {
+        setCard(response.data.data[0])
+        setLoading(false)
+      })
+      .catch(err => {
+        setCard(null)
+        alert(err.message)
+        setLoading(false)
+      })
+    
+      setName("")
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Yugioh! Search A Card</h1>
+      <p>Some cards to try: Shiba-Warrior Taro, Kuriboh, Mystical Elf, United We Stand.</p>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} placeholder='Name of card' value={name}/>
+        <button>Search</button>
+      </form>
+      <div>
+        {loading && <p>Loading...</p>}
+        {card && <div>
+          <h2>{card.name}</h2>
+          <img src={card.card_images[0].image_url}/>
+          <p>{card.desc}</p>
+          </div>}
+        
+        
+      </div>
     </div>
   );
 }
